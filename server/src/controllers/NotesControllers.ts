8bfaf7115624b481c83dbc9135bb4ef413443b5d
fatemeh zamanipour <f.zamanipour@gmail.com>
2023-07-08 11:21:17 +0330

    create notes controllers

---
+++ server/src/controllers/NotesControllers.ts
@@ -0,0 +1,121 @@
+import { Request, Response } from "express";
+import { Notes } from "../models/Notes";
+import { PostRequest } from "../routes/notes";
+
+export function getNotesByUserId(
+  userId: Request["query"]["userId"],
+  res: Response
+) {
+  Notes.findAll({
+    where: {
+      userId,
+    },
+  })
+    .then((notes) => {
+      const getNotesList = () =>
+        notes.map((note) => {
+          console.log(note);
+          return {
+            id: note.dataValues.id,
+            userId: note.dataValues.userId,
+            blocks: JSON.parse(note.dataValues.data),
+            title: note.dataValues.title,
+          };
+        });
+
+      if (notes) {
+        res.send({
+          data: getNotesList(),
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err.message);
+    });
+}
+export function getNoteById(req: Request, res: Response) {
+  const { id } = req.query;
+  if (id) {
+    Notes.findOne({
+      where: {
+        id: id,
+      },
+    })
+      .then((note) => {
+        if (note) {
+          res.status(200).send({
+            data: {
+              id: note.dataValues.id,
+              userId: note.dataValues.userId,
+              blocks: JSON.parse(note.dataValues.data),
+              title: note.dataValues.title,
+            },
+          });
+        } else {
+          res.status(404).send({
+            data: "id is not found",
+          });
+        }
+      })
+      .catch((error) => res.send({ error }));
+  } else {
+    res.status(402).send({
+      data: "id is not specified",
+    });
+  }
+}
+
+export function getNoteByQuery(
+  userId: Request["query"]["query"],
+  res: Response
+) {}
+
+export function createNote(req: PostRequest, res: Response) {
+  const { blocks, user, title } = req.body;
+  Notes.create({
+    userId: user,
+    data: blocks,
+    title,
+  })
+    .then((note) => {
+      if (note) {
+        console.log(note.dataValues);
+
+        res.send({
+          data: {
+            id: note.dataValues.id,
+            userId: note.dataValues.userId,
+            blocks: JSON.parse(note.dataValues.data),
+            title: note.dataValues.title,
+          },
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(402).send({ message: "creating note failed" });
+    });
+}
+
+export function updateNote(req: Request, res: Response) {
+  const { id, blocks, title } = req.body;
+
+  if (id) {
+    Notes.update(
+      {
+        title,
+        data: blocks,
+      },
+      {
+        where: { id: id },
+      }
+    )
+      .then((note) => {
+        res.send({
+          data: note,
+        });
+      })
+      .catch((error) => res.send({ data: error }));
+  }
+}
--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { client } from "../config/dbConfig";
-import { Notes } from "../models/Notes";
-import { UserModel } from "../models/User";
-import { Blocks } from "../models/Blocks";
-// import { OutputData } from "@editorjs/editorjs";
-// import { pool as db } from "../utils/database";
+import {
+  createNote,
+  getNoteById,
+  getNoteByQuery,
+  getNotesByUserId,
+  updateNote,
+} from "../controllers/NotesControllers";
 
 export const notesRouter = express.Router();
 export interface PostRequest extends Express.Request {
@@ -19,106 +20,24 @@ export interface PostRequest extends Express.Request {
   };
 }
 
-notesRouter.post("/api/notes/create", async (req: PostRequest, res) => {
-  const { blocks, user, title } = req.body;
-  Notes.create({
-    userId: user,
-    data: blocks,
-    title,
-  })
-    .then((note) => {
-      if (note) {
-        console.log(note.dataValues);
-
-        res.send({
-          data: {
-            id: note.dataValues.id,
-            userId: note.dataValues.userId,
-            blocks: JSON.parse(note.dataValues.data),
-            title: note.dataValues.title,
-          },
-        });
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      res.status(402).send({ message: "creating note failed" });
-    });
-});
-
-notesRouter.post("/api/notes/update", async (req, res) => {
-  const { blocks, noteId } = req.body;
-  Notes.update(
-    {
-      data: blocks,
-    },
-    {
-      where: {
-        id: noteId,
-      },
-    }
-  )
-    .then((note) => {
-      console.log(note);
-      res.send({
-        data: "updated successfully",
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      res.send({ message: "updating  note failed" });
-    });
-});
+notesRouter.post("/api/notes/create", async (req: PostRequest, res) =>
+  createNote(req, res)
+);
 
 notesRouter.get("/api/notes", async (req, res) => {
   const { userId } = req.query;
-  Notes.findAll({
-    where: {
-      userId,
-    },
-  })
-    .then((notes) => {
-      const getNotesList = () =>
-        notes.map((note) => {
-          console.log(note);
-          return {
-            id: note.dataValues.id,
-            userId: note.dataValues.userId,
-            blocks: JSON.parse(note.dataValues.data),
-            title: note.dataValues.title,
-          };
-        });
-
-      if (notes) {
-        res.send({
-          data: getNotesList(),
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err.message);
+  const { query } = req.query;
+  if (!userId) {
+    res.status(402).send({
+      message: "user is not provided",
     });
-});
-notesRouter.get("/api/note/id", async (req, res) => {
-  console.log(req.query);
-  const { id } = req.query;
-  if (id) {
-    Notes.findOne({
-      where: {
-        id: id,
-      },
-    })
-      .then((note) => {
-        res.status(200).send({
-          data: {
-            id: note.dataValues.id,
-            userId: note.dataValues.userId,
-            blocks: JSON.parse(note.dataValues.data),
-            title: note.dataValues.title,
-          },
-        });
-      })
-      .catch((error) => res.send({ error }));
+    return;
   }
+  if (!query) {
+    getNotesByUserId(userId, res);
+    return;
+  }
+  getNoteByQuery(query, res);
 });
+notesRouter.get("/api/note/id", async (req, res) => getNoteById(req, res));
+notesRouter.post("/api/note/edit", async (req, res) => updateNote(req, res));
