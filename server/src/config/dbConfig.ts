bd966115969322da464c9edb1416962e772c5e21
fatemeh zamanipour <f.zamanipour@gmail.com>
2023-07-18 15:50:56 +0330

    turn project into monorepo

--- server/src/config/dbConfig.ts
+++
@@ -1,4 +0,0 @@
-import { MongoClient } from 'mongodb'
-
-const uri = 'mongodb+srv://Elena:fzmnpr@cluster0.naxjlk1.mongodb.net/?retryWrites=true&w=majority'
-export const client = new MongoClient(uri)
